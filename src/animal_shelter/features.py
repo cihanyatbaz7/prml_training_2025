d37b64fc2e595f19e13575ae3ccfc76f7646ae88
cihanyatbaz7 <cihan.yatbaz@xebia.com>
2025-12-15 16:26:11 +0100

    Testing pytest v2

--- src/animal_shelter/features.py
+++ src/animal_shelter/features.py
@@ -96,7 +96,8 @@ def get_sex(sex_upon_outcome: pd.Series) -> pd.Series:
     """
     if not isinstance(sex_upon_outcome, pd.Series):
         raise TypeError("Input must be a pandas Series")
-    
+    print("Testing somethig")
+
     sex = pd.Series("unknown", index=sex_upon_outcome.index)
 
     sex.loc[sex_upon_outcome.str.endswith("Female")] = "female"
